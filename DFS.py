ec5d97f7218c6a4e276958872b9e98322ed57f09
Matej Gerek <matej.gerek@student.tuke.sk>
2022-12-09 16:45:06 +0100

    Add simple dfs

--- DFS.py
+++ DFS.py
@@ -1,4 +1,5 @@
 from ConsoleGui import ConsoleGui
+from Edge import Edge
 
 
 class DFS:
@@ -7,11 +8,20 @@ class DFS:
         self.gui = ConsoleGui(game)
 
     def solve(self):
-        self.game.reset()
         self.gui.draw_board()
-        self.dfs(self.game)
+        self.dfs(self.game, self.game.get_islands()[0])
+        if self.game.is_game_solved():
+            print("Solved!")
+        else:
+            print("Not solved!")
 
-    def dfs(self):
+    def dfs(self, game, starting_island):
         if self.game.is_solved():
             return True
 
+        for island in game.get_unconnected_neighbours(starting_island):
+            if self.game.add_edge_if_possible(Edge(starting_island, island)):
+                self.gui.draw_board()
+                if self.dfs(self.game, island):
+                    return True
+                self.gui.draw_board()
--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 from Game import Game
 from ConsoleGui import ConsoleGui
+from DFS import DFS
 
 # import json from games/game1.json
 import json
 
-print("Zadaj cislo levela(1-2): ", end="")
-level = input()
-game_data = json.JSONDecoder().decode(open("games/game" + level + ".json").read())
+# print("Zadaj cislo levela(1-2): ", end="")
+# level = input()
+game_data = json.JSONDecoder().decode(open("games/game" + '1' + ".json").read())
 
 game = Game(game_data['size'], game_data['islands'])
 gui = ConsoleGui(game)
-gui.draw_board()
-island1 = game.get_island_at_position(0, 0)
-island2 = game.get_island_at_position(3, 0)
-game.connect_islands(island1, island2)
-gui.draw_board()
-print('connected neighbours', game.get_connected_neighbours(island1))
-print('unconnected neighbours', game.get_unconnected_neighbours(island1))
+dfs = DFS(game)
+dfs.solve()
