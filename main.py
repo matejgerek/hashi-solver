5c016627588b09f96bf5b4c1fbc3cffe92865bac
Matej Gerek <matej.gerek@student.tuke.sk>
2022-12-13 17:11:49 +0100

    Refactor dfs from game

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Game import Game
 from ConsoleGui import ConsoleGui
+from DFS import DFS
 
 # import json from games/game1.json
 import json
@@ -10,12 +11,6 @@ game_data = json.JSONDecoder().decode(open("games/game" + level + ".json").read(
 
 game = Game(game_data['size'], game_data['islands'])
 gui = ConsoleGui(game)
+dfs = DFS(game)
+dfs.solve()
 gui.draw_board()
-Game.dfs_play_ai(game)
-gui.draw_board()
-# island1 = game.get_island_at_position(0, 0)
-# island2 = game.get_island_at_position(3, 0)
-# game.connect_islands(island1, island2)
-# gui.draw_board()
-# print('connected neighbours', game.get_connected_neighbours(island1))
-# print('unconnected neighbours', game.get_unconnected_neighbours(island1))
